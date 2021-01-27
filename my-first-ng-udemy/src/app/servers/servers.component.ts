d07918c66bc1fe594197b8b7b4b2f865928577c9
Anthony Price <antprice07@gmail.com>
2021-01-27 14:40:40 -0500

    covered databinding

--- my-first-ng-udemy/src/app/servers/servers.component.ts
+++ my-first-ng-udemy/src/app/servers/servers.component.ts
@@ -4,15 +4,34 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-servers',
   //selector: '[app-servers]',
   // selector: '.app-servers',
-  template: `
-  <app-server></app-server>
-  <app-server></app-server>`,
+  // template: `
+  // <app-server></app-server>
+  // <app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowNewServer = false;
+  serverCreationStatus = 'No Server was created!';
+  serverName = 'Testserver';
 
+
+  constructor() {
+    setTimeout(() => { 
+      this.allowNewServer = true;
+    }, 2000)
+   }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: any) {
+    console.log(event);
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+  
   ngOnInit(): void {
   }
 
