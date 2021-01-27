79eedc2826cdb15706eb003b9fc4ef54d50127f8
Anthony Price <antprice07@gmail.com>
2021-01-27 13:19:44 -0500

    started first assignment

--- basics-assignment-1-start/src/app/app.module.ts
+++ basics-assignment-1-start/src/app/app.module.ts
@@ -1,20 +1,21 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
+import { WarningalertComponent } from './warningalert/warningalert.component';
+import { SuccessalertComponent } from './successalert/successalert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent
+    WarningalertComponent,
+    SuccessalertComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
