0627bcb070d778737691e16e1b24b7d4de96d058
Anthony Price <anthonyprice@qcsoftware.com>
2021-02-01 11:48:59 -0500

    finished assignment 3 and started project

--- my-first-ng-udemy/src/app/servers/servers.component.ts
+++ my-first-ng-udemy/src/app/servers/servers.component.ts
@@ -15,6 +15,8 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No Server was created!';
   serverName = 'Testserver';
+  serverCreated = false;
+  servers = ['TestServer', 'TestServer 2'];
 
 
   constructor() {
@@ -24,6 +26,8 @@ export class ServersComponent implements OnInit {
    }
 
   onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
