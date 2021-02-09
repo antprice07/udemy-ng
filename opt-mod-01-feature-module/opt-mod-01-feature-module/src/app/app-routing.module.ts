3b82f4b8ef23c9ec45359262c1075057ec321db4
Anthony Price <antprice07@gmail.com>
2021-02-09 15:49:15 -0500

    in the middle of module section

---
+++ opt-mod-01-feature-module/opt-mod-01-feature-module/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+
+import { AuthComponent } from "./auth/auth.component";
+import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
+
+const appRoutes: Routes = [
+  { path: "", redirectTo: "/recipes", pathMatch: "full" },
+  { path: "shopping-list", component: ShoppingListComponent },
+  { path: "auth", component: AuthComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
